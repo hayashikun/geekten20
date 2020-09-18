2e4aa2b000f534d4342eed664c999e0941394f8e
Ryosuke Hayashi <hayashi@hayashikun.com>
2020-09-18 18:21:22 +0900

    Add header

---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <header id="app-header">
+    <div class="container">
+      <nav class="navbar is-fixed-top is-dark">
+        <div class="navbar-start">
+          <a class="navbar-item" href="https://github.com/hayashikun/qoin">
+            <h1>hayashikun/qoin</h1>
+          </a>
+          <a class="navbar-item" href="https://hayashikun.com/geekten20">
+            <h2>hayashikun.com/geekten20</h2>
+          </a>
+        </div>
+
+        <div class="navbar-end">
+          <h2 class="navbar-item">Ryosuke Hayashi</h2>
+          <a class="navbar-item" href="https://twitter.com/haya4p">
+            <h2>Twitter - @haya4p</h2>
+          </a>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "/node_modules/bulma/bulma.sass";
+
+#app-header {
+  .navbar-item {
+    color: white;
+  }
+  a {
+    text-decoration: none;
+  }
+  h1 {
+    font-size: 1.6em;
+    margin: 4px 10px;
+  }
+  h2 {
+    font-size: 1.2em;
+  }
+}
+</style>
