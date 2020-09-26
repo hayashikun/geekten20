e6c190d521050cc102a40f5a45d6f037d91cb5c7
Ryosuke Hayashi <hayashi@hayashikun.com>
2020-09-26 13:15:22 +0900

    remove privacy

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -6,11 +6,8 @@
       </h2>
       <div class="block">
         <ul class="myself-list">
-          <li><span>名前:</span>林 良祐</li>
           <li><span>HN:</span>はやしくんさん</li>
-          <li><span>出身:</span>大阪生まれ、大阪育ち、大阪大学学部卒</li>
-          <li><span>所属:</span>東京大学大学院 理学系研究科 化学専攻 博士後期課程2年</li>
-          <li><span>研究:</span>金属をレーザー照射によって励起したときの電子ダイナミクスの解明</li>
+          <li><span>出身:</span>大阪生まれ、大阪育ち</li>
           <li><span>趣味:</span>プログラミング</li>
           <li><span>❤️:</span>Python, Swift</li>
         </ul>
